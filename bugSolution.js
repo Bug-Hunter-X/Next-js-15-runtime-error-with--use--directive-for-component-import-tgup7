17a2a6514a2cb7cfc18b3c8b2764ec34f37c4556
BugHunter-X <askzenai@gmail.com>
2025-01-27 23:44:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,15 @@
+```javascript
+// pages/index.js
+
+import MyComponent from './MyComponent';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a paragraph.</p>
+      <MyComponent />
+    </div>
+  );
+}
+```
